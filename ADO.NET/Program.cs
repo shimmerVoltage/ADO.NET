383184de2596e72a5258b34d8d940c1f25727cc3
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-03-18 22:01:30 +0300

    Add project files.

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO.NET
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int PADDING = 30;
+			const string CONNECTION_STRING = 
+				"Data Source=(localdb)\\MSSQLLocalDB;" +
+				"Initial Catalog=Movies;" +
+				"Integrated Security=True;" +
+				"Connect Timeout=30;" +
+				"Encrypt=False;" +
+				"TrustServerCertificate=False;" +
+				"ApplicationIntent=ReadWrite;" +
+				"MultiSubnetFailover=False";
+			Console.WriteLine(CONNECTION_STRING);
+			
+			SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+
+			string cmd = "SELECT title,release_date,FORMATMESSAGE(N'%s %s',first_name,last_name) FROM Movies,Directors WHERE director=director_id";
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+
+			if (reader.HasRows)
+			{
+				Console.WriteLine("========================================================================================================");
+				for(int i = 0; i < reader.FieldCount; i++)
+					Console.Write(reader.GetName(i).PadRight(PADDING));
+				Console.WriteLine();
+				Console.WriteLine("========================================================================================================");
+				while (reader.Read())
+				{
+					//Console.WriteLine($"{reader[0].ToString().PadRight(5)}{reader[2].ToString().PadRight(15)}{reader[1].ToString().PadRight(15)}");
+					for(int i = 0; i < reader.FieldCount; i++)
+					{
+						Console.Write(reader[i].ToString().PadRight(PADDING));
+					}
+					Console.WriteLine();
+				}
+			}
+
+			reader.Close();
+			connection.Close();
+		}
+	}
+}
