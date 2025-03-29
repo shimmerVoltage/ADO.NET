77f340fd363fb24b76e68f7f129e9b2a2620472d
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-03-29 21:08:49 +0300

    RecordsCount DONE

--- Academy/Main.cs
+++ Academy/Main.cs
@@ -130,7 +130,12 @@ namespace Academy
 							"Groups,Directions",
 							$"direction=direction_id AND direction = N'{d_directions[cbGroupsDirection.SelectedItem.ToString()]}'"
 						);
-			toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}";
+			toolStripStatusLabelCount.Text = $"Количество групп: {CountRecordsInDGV(dgvGroups)}";
+		}
+
+		int CountRecordsInDGV(DataGridView dgv)
+		{
+			return dgv.Rows.Count == 0 ? 0 : dgv.RowCount - 1;
 		}
 	}
 }
