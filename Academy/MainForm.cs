59929404c9ca9331769dd639f159a4654fac86f2
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-04-17 20:25:50 +0300

    Parent Combo-Dependencies DONE

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -22,7 +22,8 @@ namespace Academy
 		public Dictionary<string, int> d_directions;
 		public Dictionary<string, int> d_groups;
 
-		public Dictionary<ComboBox, List<ComboBox>> d_dependencies; 
+		public Dictionary<ComboBox, List<ComboBox>> d_children; 
+		public Dictionary<ComboBox, List<ComboBox>> d_parents; 
 		
 		DataGridView[] tables;
 		Query[] queries = new Query[]
@@ -48,13 +49,13 @@ namespace Academy
 				),
 				new Query
 				(
-						"*",
-						"Disciplines"
+					"*",
+					"Disciplines"
 				),
 				new Query
 				(
-						"*",
-						"Teachers"
+					"*",
+					"Teachers"
 				)
 
 
@@ -74,10 +75,15 @@ namespace Academy
 		{
 			InitializeComponent();
 
-			d_dependencies = new Dictionary<ComboBox, List<ComboBox>>()
+			d_children = new Dictionary<ComboBox, List<ComboBox>>()
 			{
-				{ cbStudentsDirection, new List<ComboBox>(){ cbStudentsGroup } }				
+				{ cbStudentsDirection, new List<ComboBox>(){ cbStudentsGroup } }
 			};
+			d_parents = new Dictionary<ComboBox, List<ComboBox>>()
+			{
+				{ cbStudentsGroup, new List<ComboBox> { cbStudentsDirection } }
+			};
+
 
 			tables = new DataGridView[]
 			{
@@ -129,6 +135,8 @@ namespace Academy
 			Console.WriteLine(tab_name);
 			//int i = tabControl.SelectedIndex;
 			LoadPage(tabControl.SelectedIndex);
+			
+			#region SWITCH
 			//switch (tabControl.SelectedIndex)
 			//{ 
 			//	case 0: 
@@ -162,7 +170,9 @@ namespace Academy
 			//		dgvStudents.DataSource = connector.Select("*", "Teachers");
 			//		toolStripStatusLabelCount.Text = $"Количество преподавателев: {dgvTeachers.RowCount - 1}";
 			//	break;				
-			//}
+			//} 
+			#endregion
+
 		}		
 
 		int CountRecordsInDGV(DataGridView dgv)
@@ -206,9 +216,9 @@ namespace Academy
 			//cbStudentsGroup.Items.Clear();
 			//cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
 			
-			if (d_dependencies.ContainsKey(sender as ComboBox))
+			if (d_children.ContainsKey(sender as ComboBox))
 			{
-				foreach (ComboBox cb in d_dependencies[sender as ComboBox])
+				foreach (ComboBox cb in d_children[sender as ComboBox])
 				{
 					GetDependentData(cb, sender as ComboBox);
 				}
@@ -216,9 +226,32 @@ namespace Academy
 
 			Query query = new Query(queries[tabControl.SelectedIndex]);
 			string condition = 
-				(i == 0 || cbStudentsDirection.SelectedItem == null ? "" : $"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}");
+				(
+					i == 0 || 
+					(sender as ComboBox).SelectedItem == null ? "" : 
+					$"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}"
+				);
+			string parent_condition = "";
+			
+			if (d_parents.ContainsKey(sender as ComboBox))
+			{
+				foreach(ComboBox cb in d_parents[sender as ComboBox])
+				{
+					if (cb.SelectedItem != null && cb.SelectedIndex > 0)
+					{
+						string column_name = cb.Name.Substring(Array.FindLastIndex<char>(cb.Name.ToCharArray(), Char.IsUpper));
+						string parent_dictionary_name = $"d_{column_name.ToLower()}s";
+						Dictionary<string, int> parent_dictionary = this.GetType().GetField(parent_dictionary_name).GetValue(this) as Dictionary<string, int>;
+						if (parent_condition != "") parent_condition += " AND ";
+						parent_condition += $"[{column_name}]={parent_dictionary[cb.SelectedItem.ToString()]}"; 
+					}
+				}
+			}
+
 			if (query.Condition == "") query.Condition = condition;
 			else if (condition != "") query.Condition += $" AND {condition}";
+			if (query.Condition == "") query.Condition = parent_condition;
+			else if (parent_condition != "") query.Condition += $" AND {parent_condition}";
 			LoadPage(tabControl.SelectedIndex, query);
 		}
 
@@ -246,6 +279,8 @@ namespace Academy
 
 			dependent.Items.Clear();
 			dependent.Items.AddRange(dictionary.Select(d => d.Key).ToArray());
+			dependent.Items.Insert(0, "Все");
+			dependent.SelectedIndex = 0;
 
 			Console.WriteLine("Dependent:\t" + dependent_root);
 			Console.WriteLine("Determinant:\t" + determinant_root);
@@ -253,3 +288,4 @@ namespace Academy
 		}
 	}
 }
+
